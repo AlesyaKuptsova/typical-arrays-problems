5a9ae26f2d8523fa8cc040f3e205f49db0f99428
Alesia Kuptsova <alesya.kuptsova@tut.by>
2020-09-29 21:38:15 +0200

    feat: implement min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array !== undefined && array.length != 0) {
+        return Math.min.apply(null, array);
+    }
+    return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array !== undefined && array.length != 0) {
+        return Math.max.apply(null, array);
+    }
+    return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array !== undefined && array.length != 0) {
+        var sum=0;
+        var count = array.length;
+        for (i=0; i<count; i++) {
+            sum+= array[i];
+        }
+        return sum/count;
+    }
+    return 0;
 }
